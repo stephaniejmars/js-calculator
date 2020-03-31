66f87f4c2d9a535985fef25fd1ebb27e8af91367
Stephanie <stephaniejmars@gmail.com>
2020-03-31 16:39:30 -0700

    user input objects added

--- calculator.js
+++ calculator.js
@@ -3,3 +3,29 @@ const exampleAdditionInput = {
   num2: 5,
   operation: 'add',
 }
+
+const exampleSubtractionInput = {
+  num1: 5,
+  num2: 2,
+  operation: 'subtract'
+}
+
+const exampleMultiplicationInput = {
+  num1: 3,
+  num2: 5,
+  operation: 'multiply'
+}
+
+const exampleDivisionInput = {
+  num1: 78,
+  num2: 2, 
+  operation: 'divide'
+}
+
+// if 'add' || 'addition' || + 
+// if 'subtract' || 'subtraction' || - 
+// if 'multiply' || 'multiplication' || * 
+// if 'divide' || 'division' || /
+  // if dividing by zero 
+
+// if user input is invalid return response message 
