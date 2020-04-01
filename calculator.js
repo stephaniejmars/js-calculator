2303b88f70a74a15f9d678a1f4daba5692eaf8b8
Stephanie <stephaniejmars@gmail.com>
2020-03-31 19:52:17 -0700

    refactored using =>, ., and better outputs

--- calculator.js
+++ calculator.js
@@ -22,30 +22,31 @@ const exampleDivisionInput = {
   operation: '/'
 }
 
-const calculator = function(userInput) {
-  if (typeof userInput['num1'] !== 'number' || typeof userInput['num2'] !== 'number') {
-    console.log('Invalid entry - must enter a number')
+const calculator = (userInput) => {
+  if (typeof userInput.num1 !== 'number' || typeof userInput.num2 !== 'number') {
+    console.log('Invalid entry - must enter a number');
   } else {
       let op = userInput.operation
       if (op === 'add' || op === 'addition' || op == '+') {
-        console.log(userInput['num1'] + userInput['num2'])
+        console.log(`${userInput.num1} + ${userInput.num2} = ${userInput.num1 + userInput.num2}`);
       } else if (op === 'subtract' || op === 'subtraction' || op == '-') {
-        console.log(userInput['num1'] - userInput['num2'])
+        console.log(`${userInput.num1} - ${userInput.num2} = ${userInput.num1 - userInput.num2}`);
       } else if (op === 'multiply' || op === 'multiplication' || op == '*') {
-        console.log(userInput['num1'] * userInput['num2'])
+        console.log(`${userInput.num1} * ${userInput.num2} = ${userInput.num1 * userInput.num2}`);
       } else if (op === 'divide' || op === 'division' || op == '/') {
-          if (userInput['num2'] === 0) {
-            console.log('Cannot divide by 0')
+          if (userInput.num2 === 0) {
+            console.log('Cannot divide by 0');
           } else {
-            console.log(userInput['num1'] / userInput['num2'])
+            console.log(`${userInput.num1} / ${userInput.num2} = ${userInput.num1 / userInput.num2}`);
           }
       } else {
-        console.log('Invalid entry - must enter correct type of operator')
+        console.log('Invalid entry - must enter correct type of operator');
       }
     }
 }
 
-calculator(exampleAdditionInput)
-calculator(exampleSubtractionInput)
-calculator(exampleMultiplicationInput)
-calculator(exampleDivisionInput)
+
+calculator(exampleAdditionInput);
+calculator(exampleSubtractionInput);
+calculator(exampleMultiplicationInput);
+calculator(exampleDivisionInput);
